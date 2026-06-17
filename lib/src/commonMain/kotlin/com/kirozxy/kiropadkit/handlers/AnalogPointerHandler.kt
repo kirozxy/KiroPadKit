0f82b04de620861d918f5d88bcf1e109ed514f84
Nhat Minh (Kiro) <hanhatminh366@gmail.com>
2026-06-17 13:45:00 +0700

    init: KiroPadKit virtual gamepad library wrapper

---
+++ lib/src/commonMain/kotlin/com/kirozxy/kiropadkit/handlers/AnalogPointerHandler.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) Jam.gg 2024.
+ * Copyright (c) Nhat Minh (Kiro) 2025.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.kirozxy.kiropadkit.handlers
+
+import androidx.compose.ui.geometry.Offset
+import com.kirozxy.kiropadkit.ids.Id
+import com.kirozxy.kiropadkit.inputstate.InputState
+import com.kirozxy.kiropadkit.inputstate.setDigitalKeyIfPressed
+import com.kirozxy.kiropadkit.utils.Constants
+import com.kirozxy.kiropadkit.utils.GeometryUtils
+import com.kirozxy.kiropadkit.utils.coerceIn
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
+
+internal class AnalogPointerHandler(
+    private val directionId: Id.ContinuousDirection,
+    private val analogPressId: Id.Key?,
+) : PointerHandler {
+    data class Data(
+        var lastDownEvent: Instant = Instant.DISTANT_PAST,
+        var pressed: Boolean = false,
+        var startPosition: Offset = Offset.Unspecified,
+    )
+
+    override fun handle(
+        pointers: List<Pointer>,
+        inputState: InputState,
+        trackedIds: Set<Long>,
+        data: Any?,
+    ): Result {
+        val analogData = data as Data
+
+        val currentlyDraggedPointer =
+            pointers
+                .firstOrNull { trackedIds.contains(it.pointerId) }
+
+        return when {
+            pointers.isEmpty() -> {
+                analogData.pressed = false
+                analogData.startPosition = Offset.Unspecified
+                Result(
+                    updateInputState(inputState, Offset.Unspecified, analogData.pressed),
+                    emptySet(),
+                )
+            }
+            analogData.startPosition != Offset.Unspecified && currentlyDraggedPointer != null -> {
+                val deltaPosition =
+                    Offset(
+                        currentlyDraggedPointer.position.x - analogData.startPosition.x,
+                        analogData.startPosition.y - currentlyDraggedPointer.position.y,
+                    )
+                val offsetValue = deltaPosition.coerceIn(Offset(-1f, -1f), Offset(1f, 1f))
+                Result(
+                    updateInputState(inputState, GeometryUtils.mapCircleToSquare(offsetValue), analogData.pressed),
+                    trackedIds,
+                )
+            }
+            else -> {
+                val firstPointer = pointers.first()
+                val previousTime = analogData.lastDownEvent
+                val currentTime = Clock.System.now()
+                analogData.lastDownEvent = currentTime
+                analogData.pressed = currentTime - previousTime < Constants.DOUBLE_TAP_INTERVAL
+                analogData.startPosition = firstPointer.position
+
+                Result(updateInputState(inputState, Offset.Zero, analogData.pressed), setOf(firstPointer.pointerId))
+            }
+        }
+    }
+
+    private fun updateInputState(
+        inputState: InputState,
+        direction: Offset,
+        pressed: Boolean,
+    ): InputState {
+        var result = inputState.setContinuousDirection(directionId, direction)
+
+        if (analogPressId != null) {
+            result = result.setDigitalKeyIfPressed(analogPressId, pressed)
+        }
+
+        return result
+    }
+}
